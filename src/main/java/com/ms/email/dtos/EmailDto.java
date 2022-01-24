3ac834731ebf6df513742716a958d18cdcea9bc4
kayque <kayquegps2@gmail.com>
2022-01-24 11:57:17 -0300

    create service

--- src/main/java/com/ms/email/dtos/EmailDto.java
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -24,4 +24,46 @@ public class EmailDto {
 	
 	@NotBlank
 	private String text;
+
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+
+	public String getEmalFrom() {
+		return emalFrom;
+	}
+
+	public void setEmalFrom(String emalFrom) {
+		this.emalFrom = emalFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
 }
